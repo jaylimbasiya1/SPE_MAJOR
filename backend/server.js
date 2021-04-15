7b75990894ff6e578e3021f5aaf7dabc29366778
j-plus-plus <ljm.limbasiya@gmail.com>
2021-04-15 17:31:25 +0530

    backend of login & reg. added + validation

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 // bring routes
 const blogRoutes = require('./routes/blog');
+const authRoutes = require('./routes/auth');
 
 // app
 const app = express();
@@ -14,7 +15,10 @@ const app = express();
 // db
 mongoose
     .connect(process.env.DATABASE_LOCAL, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
-    .then(() => console.log('DB connected'));
+    .then(() => console.log('DB connected'))
+    .catch(err => {
+        console.log(err);
+    });
 
 // middlewares
 app.use(morgan('dev'));
@@ -26,6 +30,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 // routes middleware
 app.use('/api', blogRoutes);
+app.use('/api', authRoutes);
 
 // port
 const port = process.env.PORT || 8000;
