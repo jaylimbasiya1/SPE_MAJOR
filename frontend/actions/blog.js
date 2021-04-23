3a326de5443c1afe4186278e9560d4792e5cbe2a
j-plus-plus <ljm.limbasiya@gmail.com>
2021-04-23 10:25:47 +0530

    related notes componenet added

--- frontend/actions/blog.js
+++ frontend/actions/blog.js
@@ -16,12 +16,43 @@ export const createBlog = (blog, token) => {
         .catch(err => console.log(err));
 };
 
-export const listBlogsWithCategoriesAndTags = () => {
+export const listBlogsWithCategoriesAndTags = (skip, limit) => {
+    const data = {
+        limit,
+        skip
+    };
     return fetch(`http://localhost:8000/api/blogs-categories-tags`, {
         method: 'POST',
         headers: {
-            Accept: 'application/json'
-        }
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const singleBlog = slug => {
+    return fetch(`http://localhost:8000/api/blog/${slug}`, {
+        method: 'GET'
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const listRelated = blog => {
+    return fetch(`http://localhost:8000/api/blogs/related`, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(blog)
     })
         .then(response => {
             return response.json();
