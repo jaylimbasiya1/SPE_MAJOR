ed7fd0ac25b6dac2fbbdc6c1d8742c3f3278c1f6
j-plus-plus <ljm.limbasiya@gmail.com>
2021-05-11 21:05:46 +0530

    change in env variables

---
+++ frontend/next.config.js
@@ -0,0 +1,16 @@
+const withCSS = require('@zeit/next-css');
+
+module.exports={
+    publicRuntimeConfig: {
+        APP_NAME: 'spe_major',
+        //API: 'http://localhost:8000/api',
+        API_DEVELOPMENT: 'http://localhost:8000/api',
+        API_PRODUCTIONS: 'https://spemajor.com',
+        PRODUCTION: 'fasle',
+        DOMAIN_DEVELOPMENT: 'http://localhost:3000',
+        DOMAIN_PRODUCTION: 'https://spemajor.com',
+        FB_APP_ID: '43efer3f3rt3g3f',
+        GOOGLE_CLIENT_ID:'185010297137-utmvgu3tsrl3h1c3dmbn17aa4qbkjbk3.apps.googleusercontent.com'
+
+    }
+}
