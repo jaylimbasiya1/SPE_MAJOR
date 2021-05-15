50c5c9cb982ad6ded4b81e8bf16b90a7b0835904
j-plus-plus <ljm.limbasiya@gmail.com>
2021-05-15 08:39:17 +0530

    few more testcases added

--- backend/test/task.js
+++ backend/test/task.js
@@ -13,7 +13,7 @@ describe('Tasks API', () => {
      * Test the GET route
      */
     describe("GET /api/blogs", () => {
-        it("It should GET all the tasks", (done) => {
+        it("It should GET all Post", (done) => {
             chai.request(server)
                 .get("/api/blogs")
                 .end((err, response) => {
@@ -22,7 +22,7 @@ describe('Tasks API', () => {
                 });
         });
         //Wrong URI
-        it("It should NOT GET all the tasks", (done) => {
+        it("It should NOT GET any Post", (done) => {
             chai.request(server)
                 .get("/api/blogd")
                 .end((err, response) => {
@@ -32,6 +32,82 @@ describe('Tasks API', () => {
         });
 
     });
+
+    describe("GET /api/tags", () => {
+        it("It should GET all tags", (done) => {
+            chai.request(server)
+                .get("/api/tags")
+                .end((err, response) => {
+                    response.should.have.status(200);
+                done();
+                });
+        });
+        //Wrong URI
+        it("It should NOT GET Any Tags", (done) => {
+            chai.request(server)
+                .get("/api/tagss")
+                .end((err, response) => {
+                    response.should.have.status(404);
+                done();
+                });
+        });
+
+    });
+    describe("GET /api/categories", () => {
+        it("It should GET all Categories", (done) => {
+            chai.request(server)
+                .get("/api/categories")
+                .end((err, response) => {
+                    response.should.have.status(200);
+                done();
+                });
+        });
+        //Wrong URI
+        it("It should NOT GET Any categories", (done) => {
+            chai.request(server)
+                .get("/api/categoriess")
+                .end((err, response) => {
+                    response.should.have.status(404);
+                done();
+                });
+        });
+
+    });
+    describe("GET /api/blogs/search", () => {
+        
+        //
+        it("It should NOT any blog with query", (done) => {
+            chai.request(server)
+                .get("/api/blogs/searcdh")
+                .end((err, response) => {
+                    response.should.have.status(404);
+                done();
+                });
+        });
+
+    });
+
+    describe("GET /api/signnup", () => {
+        it("Unauth Singup", (done) => {
+            chai.request(server)
+                .get("/api/signnup")
+                .end((err, response) => {
+                    response.should.have.status(404);
+                done();
+                });
+        });
+        //Wrong URI
+        it("Unauth Singup wiht bad URI", (done) => {
+            chai.request(server)
+                .get("/api/signnup")
+                .end((err, response) => {
+                    response.should.have.status(404);
+                done();
+                });
+        });
+
+    });
+
 
 
    
